b5ba2cd1285f46d21222724aa13b6f9cca78be9c
ChronosX1 <938027@hazelwoodschools.org>
2020-02-26 08:20:31 -0600

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,7 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-ShadowTech = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'ShadowTech' # Only 10 chars displayed.
+team_number = '42069'
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
